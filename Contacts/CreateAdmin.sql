dd8ce36360ccec38896633667d348e818f53da55
patrickfonseca208 <patrickfonseca208@gmail.com>
2023-07-16 21:27:28 -0300

    Update repository

---
+++ Contacts/CreateAdmin.sql
@@ -0,0 +1,2 @@
+﻿insert into dbo.Users (Name, Email, Username, Password, Perfil, PerfilDescription, CreatedAt, UpdatedAt) 
+values ('SEU_NOME', 'SEU_EMAIL@PROVEDOR.com', 'SEU_USERNAME', 'SUA_SENHA', 0, 'Administrador', 'AAAA-MM-DD', NULL)
